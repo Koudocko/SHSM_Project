14d59d34f1cde6e311aee983f08caf6b9a4ae267
Koudocko <tyler@pcquest.ca>
2023-01-24 12:41:28 -0500

    Created structs for tables.

--- server/src/models.rs
+++ server/src/models.rs
@@ -1,9 +1,29 @@
-// testing with just users table
 use diesel::prelude::*;
+use diesel::sql_types::*;
 
 #[derive(Queryable)]
-pub struct User {  
-    pub id: i32,
-    pub username: String,
-    pub password: String,
+pub struct User{
+    pub id: Serial,
+    pub username: Text,
+    pub teacher: Bool,
+    pub hash: Bytea,
+    pub salt: Bytea,
+}
+
+#[derive(Queryable)]
+pub struct Announcement{
+    pub id: Serial,
+    pub title: Text,
+    pub description: Text,
+}
+
+#[derive(Queryable)]
+pub struct Event{
+    pub id: Serial,
+    pub title: Text,
+    pub description: Text,
+    pub date: Text,
+    pub certification: Bool,
+    pub completed: Bool,
+    pub user_id: Option<Integer>,
 }
