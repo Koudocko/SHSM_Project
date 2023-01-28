b4804c5b4898e176917e6565494b342391370924
Koudocko <tyler@pcquest.ca>
2023-01-28 11:04:43 -0500

    Tweaked migration.

--- server/migrations/2023-01-22-003208_database/up.sql
+++ server/migrations/2023-01-22-003208_database/up.sql
@@ -1,15 +1,20 @@
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   username TEXT NOT NULL,
-  teacher BOOLEAN NOT NULL DEFAULT FALSE,
   hash BYTEA NOT NULL,
-  salt BYTEA NOT NULL
+  salt BYTEA NOT NULL,
+  teacher BOOLEAN NOT NULL DEFAULT FALSE,
+  code TEXT DEFAULT NULL
 );
 
 CREATE TABLE announcements (
   id SERIAL PRIMARY KEY,
   title TEXT NOT NULL,
-  description TEXT NOT NULL
+  description TEXT NOT NULL,
+  user_id INT NOT NULL,
+  CONSTRAINT fk_user
+    FOREIGN KEY(user_id)
+     REFERENCES "users"(id)
 );
 
 CREATE TABLE events (
@@ -19,7 +24,7 @@ CREATE TABLE events (
   date TEXT NOT NULL,
   certification BOOLEAN NOT NULL DEFAULT FALSE,
   completed BOOLEAN NOT NULL DEFAULT FALSE,
-  user_id INT DEFAULT NULL,
+  user_id INT NOT NULL,
   CONSTRAINT fk_user
     FOREIGN KEY(user_id)
      REFERENCES "users"(id)
--- server/src/models.rs
+++ server/src/models.rs
@@ -5,9 +5,10 @@ use diesel::sql_types::*;
 pub struct User{
     pub id: Serial,
     pub username: Text,
-    pub teacher: Bool,
     pub hash: Bytea,
     pub salt: Bytea,
+    pub teacher: Bool,
+    pub code: Option<Text>,
 }
 
 #[derive(Queryable)]
@@ -15,6 +16,7 @@ pub struct Announcement{
     pub id: Serial,
     pub title: Text,
     pub description: Text,
+    pub user_id: Integer,
 }
 
 #[derive(Queryable)]
@@ -25,5 +27,5 @@ pub struct Event{
     pub date: Text,
     pub certification: Bool,
     pub completed: Bool,
-    pub user_id: Option<Integer>,
+    pub user_id: Integer,
 }
